2384b06eefd6ed97b31efea8e5c4872df46a4f29
wogudkim <76683453+wogudkim@users.noreply.github.com>
2021-11-19 14:25:44 +0900

    DayNight

--- Assets/Scripts/Controllers/Background.cs
+++ Assets/Scripts/Controllers/Background.cs
@@ -4,31 +4,49 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using Photon.Realtime;
+using System;
 
 public class Background : MonoBehaviour
 {
     public Material dayBox;
     public Material nightBox;
-    private bool isNight = false;
 
     void Update()
     {
-        //StartCoroutine(ChangeDays());
+        //ChangeDays();
+        ChageDaysWithButton();
     }
 
-    IEnumerator ChangeDays()
+    public void ChangeDays()
     {
-        yield return new WaitForSeconds(5.0f);
-        if (isNight)
+        if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 18 && RenderSettings.skybox != dayBox)
         {
             RenderSettings.skybox = dayBox;
-            isNight = false;
+            RenderSettings.ambientLight = new Color(0.5f, 0.5f, 0.5f);
         }
-        else
+        else if (DateTime.Now.Hour >= 18 && DateTime.Now.Hour < 6 && RenderSettings.skybox != nightBox)
         {
-            isNight = true;
             RenderSettings.skybox = nightBox;
+            RenderSettings.ambientLight = new Color(0.2f, 0.2f, 0.2f);
         }
 
+    }
+
+    public void ChageDaysWithButton()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (RenderSettings.skybox != dayBox)
+            {
+                RenderSettings.skybox = dayBox;
+                RenderSettings.ambientLight = new Color(0.5f, 0.5f, 0.5f);
+            }
+            else
+            {
+                RenderSettings.skybox = nightBox;
+                RenderSettings.ambientLight = new Color(0.2f, 0.2f, 0.2f);
+            }
+        }
     }
 }
