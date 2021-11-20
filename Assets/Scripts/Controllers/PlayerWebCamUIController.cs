e618758204a51b2c94a5af59d8ee27b074335654
tjrudrnr2 <70709889+tjrudrnr2@users.noreply.github.com>
2021-11-20 23:41:54 +0900

    cam

--- Assets/Scripts/Controllers/PlayerWebCamUIController.cs
+++ Assets/Scripts/Controllers/PlayerWebCamUIController.cs
@@ -1,9 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-using Photon.Pun;
-using Photon.Realtime;
+//namespace OpenCvSharp.Demo
+//{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using OpenCvSharp;
+    using OpenCvSharp.Demo;
+    using UnityEngine.UI;
+    using UnityEngine.Diagnostics;
+    using Photon.Pun;
+    using Photon.Realtime;
 
 public class PlayerWebCamUIController : MonoBehaviourPunCallbacks
 {
@@ -11,10 +20,23 @@ public class PlayerWebCamUIController : MonoBehaviourPunCallbacks
 
     public bool isWebCamDown;
 
+    bool key_flag = false;
+    Mat dst = new Mat();
+    float timer = 0f;
+    public float delayTime = 0.5f;
+    public bool isDelay = false;
+    //int circle_x = 0, circle_y = 0;
+    public RawImage display;
+    WebCamTexture camTexture;
+    private int currentIndex = 0;
+    String filenameFaceCascade =
+        "Assets/Resources/haarcascade_frontalface_default.xml";
+    CascadeClassifier faceCascade = new CascadeClassifier();
+
     // Start is called before the first frame update
     void Start()
     {
-        if(photonView.IsMine)
+        if (photonView.IsMine)
             UIManager.Instance.webCamUIController = GetComponent<PlayerWebCamUIController>();
     }
 
@@ -34,18 +56,75 @@ public class PlayerWebCamUIController : MonoBehaviourPunCallbacks
 
     public void TurnWebCam()
     {
-        if(isWebCamDown)
+        if (isWebCamDown)
         {
             photonView.RPC("TurnWebCamRPC", RpcTarget.AllBuffered);
+
+            if (key_flag == false)
+            {
+                key_flag = true;
+
+                if (camTexture != null)
+                {
+                    display.texture = null;
+                    camTexture.Stop();
+                    camTexture = null;
+                }
+                WebCamDevice device = WebCamTexture.devices[currentIndex];
+                camTexture = new WebCamTexture(device.name);
+                display.texture = camTexture;
+                camTexture.Play();
+
+                //        if (!faceCascade.Load(filenameFaceCascade))
+                //        {
+                //            Debug.Log("Video Load Error");
+                //        }
+
+                //        Mat image = new Mat();
+                //        Texture2D destTexture = new Texture2D(camTexture.width, camTexture.height, TextureFormat.ARGB32, false);
+                //        image = Unity.TextureToMat(camTexture);
+
+                //        if (!isDelay)
+                //        {
+                //            OpenCvSharp.Rect[] faces = faceCascade.DetectMultiScale(image);
+                //            foreach (var item in faces)
+                //            {
+                //                dst = image.SubMat(item);
+                //            }
+                //            isDelay = true;
+                //        }
+                //        destTexture = Unity.MatToTexture(dst);
+
+                //        display.texture = destTexture;
+
+                //        if (isDelay)
+                //        {
+                //            timer += Time.deltaTime;
+                //            if (timer >= delayTime)
+                //            {
+                //                timer = 0f;
+                //                isDelay = false;
+                //            }
+                //        }
+                //    }
+                //    else
+                //    {
+                //        key_flag = false;
+
+                //        camTexture.Stop();
+                //    }
+                //}
+            }
         }
     }
-
-    [PunRPC]
-    public void TurnWebCamRPC()
-    {
-        if (webCamImage.activeSelf)
-            webCamImage.SetActive(false);
-        else
-            webCamImage.SetActive(true);
-    }
+        [PunRPC]
+        public void TurnWebCamRPC()
+        {
+            if (webCamImage.activeSelf)
+                webCamImage.SetActive(false);
+            else
+                webCamImage.SetActive(true);
+        }
 }
+
+//}
--- Assets/Scripts/WebCam/PlayerCamController.cs
+++ Assets/Scripts/WebCam/PlayerCamController.cs
@@ -1,101 +1,100 @@
 
-namespace OpenCvSharp.Demo
-{
-    using System;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using OpenCvSharp;
-    using OpenCvSharp.Demo;
-    using UnityEngine.UI;
-    using UnityEngine.Diagnostics;
+//namespace OpenCvSharp.Demo
+//{
+//    using System;
+//    using System.Linq;
+//    using System.Text;
+//    using System.Threading.Tasks;
+//    using System.Collections;
+//    using System.Collections.Generic;
+//    using UnityEngine;
+//    using OpenCvSharp;
+//    using OpenCvSharp.Demo;
+//    using UnityEngine.UI;
+//    using UnityEngine.Diagnostics;
 
-    public class PlayerCamController : MonoBehaviour
-    {
-        bool key_flag = false;
-        Mat dst = new Mat();
-        float timer = 0f;
-        public float delayTime = 0.5f;
-        public bool isDelay = false;
-        //int circle_x = 0, circle_y = 0;
-        public RawImage display;
-        WebCamTexture camTexture;
-        private int currentIndex = 0;
-        String filenameFaceCascade =
-            "Assets/Resources/haarcascade_frontalface_default.xml";
-        CascadeClassifier faceCascade = new CascadeClassifier();
+//    public class PlayerCamController : MonoBehaviour
+//    {
+//        bool key_flag = false;
+//        Mat dst = new Mat();
+//        float timer = 0f;
+//        public float delayTime = 0.5f;
+//        public bool isDelay = false;
+//        //int circle_x = 0, circle_y = 0;
+//        public RawImage display;
+//        WebCamTexture camTexture;
+//        private int currentIndex = 0;
+//        String filenameFaceCascade =
+//            "Assets/Resources/haarcascade_frontalface_default.xml";
+//        CascadeClassifier faceCascade = new CascadeClassifier();
 
-        // Start is called before the first frame update
-        void Start()
-        {
+//        // Start is called before the first frame update
+//        void Start()
+//        {
             
-        }
+//        }
 
-        // Update is called once per frame
-        void Update()
-        {
-            Debug.Log(Input.GetKeyDown(KeyCode.C));
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                Debug.Log("test/////////////////////////");
-                if (key_flag == false)
-                {
-                    key_flag = true;
+//        // Update is called once per frame
+//        void Update()
+//        {
+//            Debug.Log(Input.GetKeyDown(KeyCode.C));
+//            if (Input.GetKeyDown(KeyCode.C))
+//            {
+//                if (key_flag == false)
+//                {
+//                    key_flag = true;
 
-                    if (camTexture != null)
-                    {
-                        display.texture = null;
-                        camTexture.Stop();
-                        camTexture = null;
-                    }
+//                    if (camTexture != null)
+//                    {
+//                        display.texture = null;
+//                        camTexture.Stop();
+//                        camTexture = null;
+//                    }
 
-                    WebCamDevice device = WebCamTexture.devices[currentIndex];
-                    camTexture = new WebCamTexture(device.name);
+//                    WebCamDevice device = WebCamTexture.devices[currentIndex];
+//                    camTexture = new WebCamTexture(device.name);
                     
-                    camTexture.Play();
+//                    camTexture.Play();
 
-                    if (!faceCascade.Load(filenameFaceCascade))
-                    {
-                        Debug.Log("Video Load Error");
-                    }
+//                    if (!faceCascade.Load(filenameFaceCascade))
+//                    {
+//                        Debug.Log("Video Load Error");
+//                    }
 
-                    Mat image = new Mat();
-                    Texture2D destTexture = new Texture2D(camTexture.width, camTexture.height, TextureFormat.ARGB32, false);
-                    image = Unity.TextureToMat(camTexture);
+//                    Mat image = new Mat();
+//                    Texture2D destTexture = new Texture2D(camTexture.width, camTexture.height, TextureFormat.ARGB32, false);
+//                    image = Unity.TextureToMat(camTexture);
 
-                    if (!isDelay)
-                    {
-                        OpenCvSharp.Rect[] faces = faceCascade.DetectMultiScale(image);
-                        foreach (var item in faces)
-                        {
-                            dst = image.SubMat(item);
-                        }
-                        isDelay = true;
-                    }
-                    destTexture = Unity.MatToTexture(dst);
+//                    if (!isDelay)
+//                    {
+//                        OpenCvSharp.Rect[] faces = faceCascade.DetectMultiScale(image);
+//                        foreach (var item in faces)
+//                        {
+//                            dst = image.SubMat(item);
+//                        }
+//                        isDelay = true;
+//                    }
+//                    destTexture = Unity.MatToTexture(dst);
 
-                    display.texture = destTexture;
+//                    display.texture = destTexture;
 
-                    if (isDelay)
-                    {
-                        timer += Time.deltaTime;
-                        if (timer >= delayTime)
-                        {
-                            timer = 0f;
-                            isDelay = false;
-                        }
-                    }
-                }
-                else
-                {
-                    key_flag = false;
+//                    if (isDelay)
+//                    {
+//                        timer += Time.deltaTime;
+//                        if (timer >= delayTime)
+//                        {
+//                            timer = 0f;
+//                            isDelay = false;
+//                        }
+//                    }
+//                }
+//                else
+//                {
+//                    key_flag = false;
 
-                    camTexture.Stop();
-                }
-            }
-        }
-    }
-}
+//                    camTexture.Stop();
+//                }
+//            }
+//        }
+//    }
+//}
