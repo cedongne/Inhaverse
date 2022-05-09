6bcd51f4359de247dfed23c0288c00d5d60b0250
cedongne <cedongne@gmail.com>
2022-05-09 18:19:34 +0900

    Optimization

--- Assets/Scripts/Managers/RpcUIManager.cs
+++ Assets/Scripts/Managers/RpcUIManager.cs
@@ -35,15 +35,4 @@ public class RpcUIManager : MonoBehaviour
             instance = GetComponent<RpcUIManager>();
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
